44cd4d5845f5feb98dee546cab037eddba70420f
Laura Larregui <laura9379@hotmail.com>
2020-07-30 22:17:53 -0500

    update 7/30/2020 - added regression trees

--- IST707FinalProjectCode.R
+++ IST707FinalProjectCode.R
@@ -46,7 +46,7 @@ str(heart)#test
 #Recoding Variables
 heart$thal=dplyr::recode(heart$thal, "0"="Normal", "1"="Fixed Defect", "2"="Reversable Defect")
 #head(heart$thal)# I see there is another level "3" which wasn't mentioned in the data info
-heart$thal=dplyr::recode(heart$thal,"3"="Unreversable Defect") #I'm assuming this is the value behind 3
+heart$thal=dplyr::recode(heart$thal,"3"="") #I'm assuming this is the value behind 3
 heart$cp=dplyr::recode(heart$cp, "0"="None", "1"="Low", "2"="Medium","3"="High")
 #head(heart$cp)
 heart$target=dplyr::recode(heart$target, "0"="low risk", "1"="high risk")
@@ -92,3 +92,29 @@ rulesHeartRight <- apriori(newheart, parameter = list(supp = suppVar, conf = con
                          appearance = list (default = "lhs", rhs= "target=high risk" ),control=list(verbose=F))
 options(digits=2)
 inspect(rulesHeartRight)
+
+################################
+#Recursive Partitioning and Regression Trees 
+library(rpart,warn.conflicts = FALSE) 
+library(rpart.plot,warn.conflicts = FALSE) 
+library(rattle, warn.conflicts = FALSE) 
+## Rattle: A free graphical interface for data science with R. 
+## Version 5.2.0 Copyright (c) 2006-2018 Togaware Pty Ltd. 
+## Type 'rattle()' to shake, rattle, and roll your data. 
+#Recursive Partitioning and Regression Trees  
+nrows<-nrow(heart) 
+cutPoint<- floor(nrows/3*2) 
+cutPoint 
+## [1] 201 
+rand<-sample(1:nrows) 
+#training set 
+heart_train <- heart[rand[1:cutPoint],] 
+#test set 
+heart_test <- heart[rand[(cutPoint+1:nrows)],] 
+heart_test<-na.omit(heart_test) 
+w.rpart <- rpart(target ~. , data = heart_train)
+w.rpart
+rpart.plot(w.rpart, digits = 3)
+fancyRpartPlot (w.rpart)
+
+
